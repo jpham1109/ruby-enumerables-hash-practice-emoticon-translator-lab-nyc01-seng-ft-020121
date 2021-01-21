bba2b17ecab70fe60d142e871a63643c8278ebef
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-21 02:44:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,6 @@ file = YAML.load_file('./lib/emoticons.yml')
 puts file.inspect
 
 def load_library(file)
-  file = YAML.load_file('./lib/emoticons.yml')
   emoticons = file.each_with_object({}) do |(k, v), emoticons|
     emoticons[k] = Hash[:english, v[0], :japanese, v[1]]
   end
